a0f300b4d185b24ed6add11309968d880bfe740b
Seun Ogunsanya <seun.ogunsanya@ss-limited.com>
2020-08-26 01:56:54 +0100

    change base url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const storage_type = window.localStorage;
-export const BASE_URL = "http://127.0.0.1:5000";
-// export const BASE_URL = "https://second-best-todo.herokuapp.com";
+// export const BASE_URL = "http://127.0.0.1:5000";
+export const BASE_URL = "https://second-best-todo.herokuapp.com";
 export const token = "pntnYA7DqO4G0mwz3QY";
